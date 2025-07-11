5c403329b283f4cab7a2bc3c1010298f14a2bdfa
Ananya Saini <ananyasaini050@gmail.com>
2025-07-12 00:35:34 +0530

    Time: 2 ms (83.09%) | Memory: 45.6 MB (35.98%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.java
+++ 130-surrounded-regions/surrounded-regions.java
@@ -1,26 +1,23 @@
 class Solution {
     public void solve(char[][] board) {
-        // Base condition
         if(board.length == 0) return;
-        // 1st Loop : Traversing over top column & bottom column, to find any 'O' present by the boundary
-        for(int i = 0; i < board[0].length; i++){
+
+        for(int i = 0; i<board[0].length; i++){
             if(board[0][i] == 'O'){
-                DFS(board, 0, i);
+                dfs(board, 0, i);
             }
-            if(board[board.length - 1][i] == 'O'){
-                DFS(board, board.length - 1, i);
+            if(board[board.length-1][i] == 'O'){
+                dfs(board, board.length-1, i);
             }
         }
-        // 2nd Loop : Traversing over left row & right row, to find any 'O' present by the boundary
         for(int i = 0; i < board.length; i++){
             if(board[i][0] == 'O'){
-                DFS(board, i, 0);
+                dfs(board, i, 0);
             }
-            if(board[i][board[0].length - 1] == 'O'){
-                DFS(board, i, board[0].length - 1);
+            if(board[i][board[0].length-1] == 'O'){
+                dfs(board, i, board[0].length-1);
             }
         }
-        // 3rd Loop : Now in this we will traverse on each n every node & check if they are 'O' convert into 'X', if they are '@' convert into 'O'
         for(int i = 0; i < board.length; i++){
             for(int j = 0; j < board[0].length; j++){
                 if(board[i][j] == 'O'){
@@ -33,15 +30,15 @@ class Solution {
         }
         return;
     }
-    // This calls helps in convert the 'O' node present near by the boundary convert them into '@'
-    public void DFS(char[][] board, int i, int j){
-        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O'){
+
+    void dfs(char[][] board, int i, int j){
+        if(i<0 || i >= board.length || j<0 || j>= board[0].length || board[i][j] != 'O'){
             return;
         }
         board[i][j] = '@';
-        DFS(board, i + 1, j);
-        DFS(board, i - 1, j);
-        DFS(board, i, j + 1);
-        DFS(board, i, j - 1);
+        dfs(board, i+1, j);
+        dfs(board, i-1, j);
+        dfs(board, i, j+1);
+        dfs(board, i, j-1);
     }
 }
