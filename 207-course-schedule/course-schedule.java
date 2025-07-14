aad199d5f82ec33e6c195cf1ca82501cec14a547
Ananya Saini <ananyasaini050@gmail.com>
2025-07-15 00:17:54 +0530

    Time: 6 ms (73.68%) | Memory: 45.8 MB (27.84%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer> result = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 0;
+
+        for(int i = 0; i < numCourses; i++){
+            graph.add(new ArrayList<>());
+        }
+        for(int[] prerequisite : prerequisites){
+            graph.get(prerequisite[0]).add(prerequisite[1]);
+        }
+
+        int[] indegree = new int[numCourses];
+        for(int u = 0; u < numCourses; u++){
+            for(int v : graph.get(u)){
+                indegree[v]++;
+            }
+        }
+        for(int i = 0; i < numCourses; i++){
+            if(indegree[i] == 0){
+                queue.add(i);
+                count++;
+            }
+        }
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            result.add(curr);
+            for(int neighbour : graph.get(curr)){
+                indegree[neighbour]--;
+                if(indegree[neighbour] == 0){
+                    queue.add(neighbour);
+                    count++;
+                }
+            }
+        }
+        if(count == numCourses){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
