74695d7c5764fac93acc51988b0767f4a4350f82
Ananya Saini <ananyasaini050@gmail.com>
2025-08-18 02:23:16 +0530

    Time: 7 ms (39.44%) | Memory: 45.6 MB (19.50%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.java
+++ 735-asteroid-collision/asteroid-collision.java
@@ -3,15 +3,13 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
         int n = asteroids.length;
         boolean isSameSize = false;
-
         for(int i = 0; i < n; i++){
             isSameSize = false;
             while(!stack.isEmpty() && (stack.peek() > 0 && asteroids[i] < 0)){
                 if(Math.abs(asteroids[i]) > stack.peek()){
                     stack.pop();
                     continue;
-                }
-                else if(Math.abs(asteroids[i]) == stack.peek()){
+                }else if(Math.abs(asteroids[i]) == stack.peek()){
                     stack.pop();
                 }
                 isSameSize = true;
@@ -19,11 +17,10 @@ class Solution {
             }
             if(!isSameSize){
                 stack.push(asteroids[i]);
-            }
+            } 
         }
         int[] result = new int[stack.size()];
         for(int i = stack.size()-1; i >= 0; i--){
-            
             result[i] = stack.pop();
         }
         return result;
