19644e01c95e70efeba4e54f1d27fbff31e6f868
Ananya Saini <ananyasaini050@gmail.com>
2025-09-08 02:40:32 +0530

    Time: 200 ms (94.82%) | Memory: 0B (100.00%) - LeetSync

--- 183-customers-who-never-order/customers-who-never-order.sql
+++ 183-customers-who-never-order/customers-who-never-order.sql
@@ -1,10 +1,10 @@
 -- Write your PostgreSQL query statement below
--- select c.name as customers
--- from customers c
--- left join orders o
--- on c.id = o.customerid
--- where o.id is null;
+select c.name as customers
+from customers c
+left join orders o
+on c.id = o.customerid
+where o.id is null;
 
-select name as customers 
-from customers
-where id not in (select customerid from orders);
+-- select name as customers 
+-- from customers
+-- where id not in (select customerid from orders);
